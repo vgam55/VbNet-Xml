2f32a69d142beaf1015abdac5c6ec6f66bcce325
unknown <vgam55@gmail.com>
2020-03-05 17:44:01 +0100

    04/03/2020

---
+++ PruebaXml/Form1.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListadoNodos = New System.Windows.Forms.DataGridView()
+        Me.Id_autor = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.apellidos = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.nombre = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.telefono = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.direccion = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ciudad = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.provincia = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.codigo_postal = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.contrato = New System.Windows.Forms.DataGridViewCheckBoxColumn()
+        Me.tbSalidaXML = New System.Windows.Forms.TextBox()
+        Me.btnLeerXML = New System.Windows.Forms.Button()
+        Me.btnMostrarEsquema = New System.Windows.Forms.Button()
+        CType(Me.ListadoNodos, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ListadoNodos
+        '
+        Me.ListadoNodos.AllowUserToAddRows = False
+        Me.ListadoNodos.AllowUserToDeleteRows = False
+        Me.ListadoNodos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.ListadoNodos.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Id_autor, Me.apellidos, Me.nombre, Me.telefono, Me.direccion, Me.ciudad, Me.provincia, Me.codigo_postal, Me.contrato})
+        Me.ListadoNodos.Location = New System.Drawing.Point(3, 3)
+        Me.ListadoNodos.Name = "ListadoNodos"
+        Me.ListadoNodos.ReadOnly = True
+        Me.ListadoNodos.Size = New System.Drawing.Size(916, 150)
+        Me.ListadoNodos.TabIndex = 0
+        '
+        'Id_autor
+        '
+        Me.Id_autor.HeaderText = "Id Autor"
+        Me.Id_autor.Name = "Id_autor"
+        Me.Id_autor.ReadOnly = True
+        '
+        'apellidos
+        '
+        Me.apellidos.HeaderText = "Apellidos"
+        Me.apellidos.Name = "apellidos"
+        Me.apellidos.ReadOnly = True
+        '
+        'nombre
+        '
+        Me.nombre.HeaderText = "Nombre"
+        Me.nombre.Name = "nombre"
+        Me.nombre.ReadOnly = True
+        '
+        'telefono
+        '
+        Me.telefono.HeaderText = "Telefono"
+        Me.telefono.Name = "telefono"
+        Me.telefono.ReadOnly = True
+        '
+        'direccion
+        '
+        Me.direccion.HeaderText = "Dirección"
+        Me.direccion.Name = "direccion"
+        Me.direccion.ReadOnly = True
+        '
+        'ciudad
+        '
+        Me.ciudad.HeaderText = "Ciudad"
+        Me.ciudad.Name = "ciudad"
+        Me.ciudad.ReadOnly = True
+        '
+        'provincia
+        '
+        Me.provincia.HeaderText = "Provincia"
+        Me.provincia.Name = "provincia"
+        Me.provincia.ReadOnly = True
+        '
+        'codigo_postal
+        '
+        Me.codigo_postal.HeaderText = "Código Postal"
+        Me.codigo_postal.Name = "codigo_postal"
+        Me.codigo_postal.ReadOnly = True
+        '
+        'contrato
+        '
+        Me.contrato.HeaderText = "Contrato"
+        Me.contrato.Name = "contrato"
+        Me.contrato.ReadOnly = True
+        '
+        'tbSalidaXML
+        '
+        Me.tbSalidaXML.Location = New System.Drawing.Point(13, 169)
+        Me.tbSalidaXML.Multiline = True
+        Me.tbSalidaXML.Name = "tbSalidaXML"
+        Me.tbSalidaXML.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.tbSalidaXML.Size = New System.Drawing.Size(906, 86)
+        Me.tbSalidaXML.TabIndex = 1
+        Me.tbSalidaXML.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'btnLeerXML
+        '
+        Me.btnLeerXML.Location = New System.Drawing.Point(209, 280)
+        Me.btnLeerXML.Name = "btnLeerXML"
+        Me.btnLeerXML.Size = New System.Drawing.Size(88, 33)
+        Me.btnLeerXML.TabIndex = 2
+        Me.btnLeerXML.Text = "Leer XML"
+        Me.btnLeerXML.UseVisualStyleBackColor = True
+        '
+        'btnMostrarEsquema
+        '
+        Me.btnMostrarEsquema.Location = New System.Drawing.Point(631, 280)
+        Me.btnMostrarEsquema.Name = "btnMostrarEsquema"
+        Me.btnMostrarEsquema.Size = New System.Drawing.Size(101, 33)
+        Me.btnMostrarEsquema.TabIndex = 3
+        Me.btnMostrarEsquema.Text = "Mostrar Esquema"
+        Me.btnMostrarEsquema.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(931, 450)
+        Me.Controls.Add(Me.btnMostrarEsquema)
+        Me.Controls.Add(Me.btnLeerXML)
+        Me.Controls.Add(Me.tbSalidaXML)
+        Me.Controls.Add(Me.ListadoNodos)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.ListadoNodos, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ListadoNodos As DataGridView
+    Friend WithEvents Id_autor As DataGridViewTextBoxColumn
+    Friend WithEvents apellidos As DataGridViewTextBoxColumn
+    Friend WithEvents nombre As DataGridViewTextBoxColumn
+    Friend WithEvents telefono As DataGridViewTextBoxColumn
+    Friend WithEvents direccion As DataGridViewTextBoxColumn
+    Friend WithEvents ciudad As DataGridViewTextBoxColumn
+    Friend WithEvents provincia As DataGridViewTextBoxColumn
+    Friend WithEvents codigo_postal As DataGridViewTextBoxColumn
+    Friend WithEvents contrato As DataGridViewCheckBoxColumn
+    Friend WithEvents tbSalidaXML As TextBox
+    Friend WithEvents btnLeerXML As Button
+    Friend WithEvents btnMostrarEsquema As Button
+End Class
---
+++ PruebaXml/Form1.vb
@@ -0,0 +1,51 @@
+﻿Imports System.Xml
+
+'Ejemplo de como leer un archivo XML
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    'En esta función leemos el archivo como texto plano
+    Private Sub btnLeerXML_Click(sender As Object, e As EventArgs) Handles btnLeerXML.Click
+        Dim lectorXML As New XmlTextReader("C:\Users\redViti\source\repos\PruebaXml\Autores.xml")
+        While lectorXML.Read()
+            'XMLReader distinge entre nodo, contenido del nodo y elemento final
+            Select Case lectorXML.NodeType
+                Case XmlNodeType.Element 'nodo
+                    tbSalidaXML.Text = tbSalidaXML.Text + "<" + lectorXML.Name & ">\n"
+                    Exit Select
+                Case XmlNodeType.Text 'contenido del nodo
+                    tbSalidaXML.Text = tbSalidaXML.Text + lectorXML.Value
+                    Exit Select
+                Case XmlNodeType.EndElement 'elemento final
+                    tbSalidaXML.Text = tbSalidaXML.Text + ""
+                    Exit Select
+            End Select
+        End While
+    End Sub
+
+    'En esta función utilizamos XmlDocument para cargar el documento XML, elegir los nodos que queremos leer,
+    'recorrer la lista y mostrarlo en un DataGridView
+    Private Sub btnMostrarEsquema_Click(sender As Object, e As EventArgs) Handles btnMostrarEsquema.Click
+        Dim documentoXML = New XmlDocument()  'Guarda el doxumento XML
+        Dim listaNodos As XmlNodeList 'Guarda la lista de nodos que quermos recorrer
+        Dim nodo As XmlNode 'Guarda un nodo concreto
+        Dim cont As Integer
+        Dim cont1 As Integer
+        documentoXML.Load("C:\Users\redViti\source\repos\PruebaXml\Autores.xml") 'Cargamos el archivo XML
+        listaNodos = documentoXML.SelectNodes("/Authors_Table/authors") 'Seleccionmaos la lista de nodos
+        For cont = 0 To listaNodos.Count() - 1 'Recorremos la lista de nodos
+            nodo = listaNodos.Item(cont) 'Cojemos el nodo que nos interesa
+            ListadoNodos.Rows.Add() 'Creamos una nueva fila en el DataGridView
+            For cont1 = 0 To 8
+                'Recorremos los elementos que forman el nodo y mostramos sus contenido
+                ListadoNodos.Rows(cont).Cells(cont1).Value = nodo.ChildNodes.Item(cont1).InnerText
+            Next
+
+        Next
+
+
+    End Sub
+End Class
